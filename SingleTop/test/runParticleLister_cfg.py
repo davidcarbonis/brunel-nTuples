d005bae8d735297b89916ad924c935d1d4182fd7
Alexander <alexander.david.morton@cern.ch>
2015-08-11 14:56:50 +0100

    Updated file directory structure to reflect how the directory structure is in CMSSW in git

---
+++ SingleTop/test/runParticleLister_cfg.py
@@ -0,0 +1,29 @@
+#Setting up various environmental stuff that makes all of this jazz actually work.
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing ('python')
+options.setDefault('maxEvents', 3)
+options.parseArguments()
+
+process = cms.Process("USER")
+
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+	'file:/nfs/data/eepgadm/ROOTfiles/Summer12_tZq_signal_MC/001C0B68-536A-E311-B25F-002590D0B066.root',	
+        )
+)
+
+## Events to process
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
+
+
+process.printTree = cms.EDAnalyzer("ParticleListDrawer",
+  maxEventsToPrint = cms.untracked.int32(3),
+  printVertex = cms.untracked.bool(False),
+  src = cms.InputTag("genParticles")
+)
+
+process.p = cms.Path(process.printTree)
