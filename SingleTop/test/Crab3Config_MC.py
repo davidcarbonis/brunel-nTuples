149f7f76835b54aeb699984bd33c8446fa89cb99
Alexander Morton <alexander.david.morton.cern.ch>
2015-08-13 15:27:58 +0200

    Updated additional input files parameter

--- SingleTop/test/Crab3Config_MC.py
+++ SingleTop/test/Crab3Config_MC.py
@@ -6,15 +6,14 @@ config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'nTupliserMC_cfg.py'
+config.JobType.inputFiles = ['pileup_MC_Summer12.root', 'run2012C_v2.root', 'run2012A_13Jul.root', 'run2012B_13Jul.root']
+
 
 config.Data.inputDataset = '/WZJetsTo3LNu_scaledown_8TeV_TuneZ2Star_madgraph_tauola/Summer12_DR53X-PU_S10_START53_V19-v1/AODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 10
 config.Data.outLFNDirBase = '/store/user/%s/nTuples/WZ/150813/MC/test' % (getUsernameFromSiteDB())
-
-#config.Data.outLFN = '/store/user/almorton/nTuples/WZ/150813/MC/test/'
-#config.Data.outLFN = '/store/user/<subdir>' # or '/store/group/<subdir>'
 
 config.Data.publication = False
 config.Data.publishDataName = 'CRAB3_MC_nTupilisation_WZ_test_150813'
