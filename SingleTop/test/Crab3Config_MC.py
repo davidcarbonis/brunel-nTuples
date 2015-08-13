b0161c4839c8807d2072b68149ba9927a247c95c
Alexander Morton <alexander.david.morton.cern.ch>
2015-08-13 14:47:58 +0200

    Updated Crab3Config file for MC

--- SingleTop/test/Crab3Config_MC.py
+++ SingleTop/test/Crab3Config_MC.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'WZJets_ScaleDown_MC_150306'
+config.General.requestName = 'WZJets_ScaleDown_MC_150813'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
@@ -11,12 +11,13 @@ config.Data.inputDataset = '/WZJetsTo3LNu_scaledown_8TeV_TuneZ2Star_madgraph_tau
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 10
-config.Data.outLFN = '/store/user/almorton/nTuples/WT/150306/MC/'
+config.Data.outLFNDirBase = '/store/user/%s/nTuples/WZ/150813/MC/test' % (getUsernameFromSiteDB())
 
+#config.Data.outLFN = '/store/user/almorton/nTuples/WZ/150813/MC/test/'
 #config.Data.outLFN = '/store/user/<subdir>' # or '/store/group/<subdir>'
 
 config.Data.publication = False
-config.Data.publishDataName = 'CRAB3_MC_nTupilisation_ScaleDown_150306'
+config.Data.publishDataName = 'CRAB3_MC_nTupilisation_WZ_test_150813'
 
-config.Site.storageSite = T2_UK_London_Brunel
+config.Site.storageSite = 'T2_UK_London_Brunel'
 
