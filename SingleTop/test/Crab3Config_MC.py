47d7c12d3f8ab48b97e691d6e80e3e8743645c69
Alexander <alexander.david.morton@cern.ch>
2015-09-28 13:45:25 +0100

    Corrected syntax error.

--- SingleTop/test/Crab3Config_MC.py
+++ SingleTop/test/Crab3Config_MC.py
@@ -1,22 +1,22 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'WZJets_ScaleDown_MC_150306'
+config.General.requestName = 't-chan_5f_MC_150928'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'nTupliserMC_miniAOD_cfg.py'
 
-config.Data.inputDataset = '/WZJetsTo3LNu_scaledown_8TeV_TuneZ2Star_madgraph_tauola/Summer12_DR53X-PU_S10_START53_V19-v1/AODSIM'
+config.Data.inputDataset = '/ST_t-channel_5f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 10
-config.Data.outLFN = '/store/user/almorton/nTuples/WT/150306/MC/'
+config.Data.outLFN = '/store/user/almorton/nTuples/t-chan_5f/150928/MC/'
 
 #config.Data.outLFN = '/store/user/<subdir>' # or '/store/group/<subdir>'
 
 config.Data.publication = False
-config.Data.publishDataName = 'CRAB3_MC_nTupilisation_ScaleDown_150306'
+config.Data.publishDataName = 'CRAB3_MC_nTupilisation_t-chan_5f_150928'
 
-config.Site.storageSite = T2_UK_London_Brunel
+config.Site.storageSite = 'T2_UK_London_Brunel'
 
