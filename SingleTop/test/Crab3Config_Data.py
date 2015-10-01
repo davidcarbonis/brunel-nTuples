0f9b233f33e9b3c48b9cf03c394e16386d83c7de
Alexander Morton <alexander.david.morton.cern.ch>
2015-10-01 16:06:35 +0200

    Corrected typos.

--- SingleTop/test/Crab3Config_Data.py
+++ SingleTop/test/Crab3Config_Data.py
@@ -12,11 +12,10 @@ config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 20
 config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-255031_13TeV_PromptReco_Collisions15_50ns_JSON_v2.txt'
-config.Data.runRange = 253659-256464
+config.Data.runRange = '253659-256464'
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
 config.Data.publication = False
 config.Data.publishDataName = 'CRAB3_Data_nTupilisation_DoubleMuon_Run2015C_Golden38T25ns_151001'
 
-config.Site.storageSite = T2_UK_London_Brunel
-
+config.Site.storageSite = 'T2_UK_London_Brunel'
