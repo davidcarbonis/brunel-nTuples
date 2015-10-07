2dba4dbf3528b4acd241282966f0546aae63c327
Alexander Morton <alexander.david.morton.cern.ch>
2015-10-07 16:17:47 +0200

    Updated Crab3Config_Data.py

--- SingleTop/test/Crab3Config_Data.py
+++ SingleTop/test/Crab3Config_Data.py
@@ -1,22 +1,21 @@
-from CRABClient.UserUtilities import config
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'WZJets_ScaleDown_MC_150306'
+config.General.requestName = 'DoubleEG_Run2015B_Jul2015_Golden38T50ns_Data_151005b'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'nTupliserData_miniAOD_cfg.py'
 
-config.Data.inputDataset = '/WZJetsTo3LNu_scaledown_8TeV_TuneZ2Star_madgraph_tauola/Summer12_DR53X-PU_S10_START53_V19-v1/AODSIM'
+config.Data.inputDataset = '/DoubleEG/Run2015B-17Jul2015-v1/MINIAOD'
 config.Data.inputDBS = 'global'
-config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 10
-config.Data.outLFN = '/store/user/almorton/nTuples/WT/150306/MC/'
-
-#config.Data.outLFN = '/store/user/<subdir>' # or '/store/group/<subdir>'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 1
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-255031_13TeV_PromptReco_Collisions15_50ns_JSON_v2.txt'
+config.Data.runRange = '247607-253620'
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
 config.Data.publication = False
-config.Data.publishDataName = 'CRAB3_MC_nTupilisation_ScaleDown_150306'
+config.Data.publishDataName = 'CRAB3_Data_nTupilisation_DoubleEG_Run2015B_Jul2015_Golden38T50ns_151005b'
 
-config.Site.storageSite = T2_UK_London_Brunel
-
+config.Site.storageSite = 'T2_UK_London_Brunel'
