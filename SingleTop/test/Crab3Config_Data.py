473ff7be95ff61fe801fd5bb2258c0aeb5f5bd3e
Alexander <alexander.david.morton@cern.ch>
2015-12-02 10:06:42 +0000

    Updated to use latest/final JSON lumi file for Run2015C and Run2015D.

--- SingleTop/test/Crab3Config_Data.py
+++ SingleTop/test/Crab3Config_Data.py
@@ -1,21 +1,21 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'DoubleEG_Run2015C_05Oct2015_Golden38T_Data_151101'
+config.General.requestName = 'DoubleEG_Run2015D_05Oct2015_Golden38T_Data_151202'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'nTupliserData_miniAOD_cfg.py'
 
-config.Data.inputDataset = '/DoubleEG/Run2015C_25ns-05Oct2015-v1/MINIAOD'
+config.Data.inputDataset = '/DoubleEG/Run2015D-05Oct2015-v1/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 10
-config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-259891_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
-config.Data.runRange = '253659-256464'
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
+config.Data.runRange = '256630-258158'
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
 config.Data.publication = False
-config.Data.outputDatasetTag = 'CRAB3_Data_nTupilisation_DoubleEG_Run2015C_05Oct2015_Golden38T_151101'
+config.Data.outputDatasetTag = 'CRAB3_Data_nTupilisation_DoubleEG_Run2015D_05Oct2015_Golden38T_151202'
 
 config.Site.storageSite = 'T2_UK_London_Brunel'
