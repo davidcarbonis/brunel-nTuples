d3c09d81dd98761ec5a3749d1c33ddaf5b15040e
Alexander Morton <alexander.david.morton.cern.ch>
2016-01-08 15:36:04 +0100

    Updated sample Crab3Config files, using 7_6_3 MINIAOD(SIM) input which should work for 7_6_3 in  practice.

--- SingleTop/test/Crab3Config_Data.py
+++ SingleTop/test/Crab3Config_Data.py
@@ -1,21 +1,21 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'DoubleEG_Run2015D_05Oct2015_Golden38T_Data_151202'
+config.General.requestName = 'DoubleEG_Run2015C_16Dec2015_Golden38T_Data_160108'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'nTupliserData_miniAOD_cfg.py'
 
-config.Data.inputDataset = '/DoubleEG/Run2015D-05Oct2015-v1/MINIAOD'
+config.Data.inputDataset = '/DoubleEG/Run2015C_25ns-16Dec2015-v1/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 10
-config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
-config.Data.runRange = '256630-258158'
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_v2.txt'
+config.Data.runRange = '256630-258158' #Run2015C 253659-256464; Run2015D 256630-260627
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
 config.Data.publication = False
-config.Data.outputDatasetTag = 'CRAB3_Data_nTupilisation_DoubleEG_Run2015D_05Oct2015_Golden38T_151202'
+config.Data.outputDatasetTag = 'CRAB3_Data_nTupilisation_DoubleEG_Run2015C_16Dec2015_Golden38T_160108'
 
 config.Site.storageSite = 'T2_UK_London_Brunel'
--- SingleTop/test/Crab3Config_MC.py
+++ SingleTop/test/Crab3Config_MC.py
@@ -2,21 +2,21 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 
 config = config()
 
-config.General.requestName = 'ttJets_ext_151031'
+config.General.requestName = 'ZZTo4L_160108'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'nTupliserMC_miniAOD_cfg.py'
 config.JobType.inputFiles = ['pileup_MC_Summer12.root', 'run2012A_13Jul.root', 'run2012B_13Jul.root', 'run2012C_v2.root']
 
-config.Data.inputDataset = '/TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2_ext1-v1/MINIAODSIM'
+config.Data.inputDataset = '/ZZTo4L_13TeV_powheg_pythia8/RunIIFall15MiniAODv1-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
 config.Data.publication = False
-config.Data.outputDatasetTag = 'CRAB3_MC_nTupilisation_ttJets_ext_151031'
+config.Data.outputDatasetTag = 'CRAB3_MC_nTupilisation_ZZTo4L_160108'
 
 config.Site.storageSite = 'T2_UK_London_Brunel'
 
