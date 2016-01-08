63d4b1039d3d2dd5897a05a4d312e835ef6f32e4
Alexander Morton <alexander.david.morton.cern.ch>
2016-01-08 15:45:38 +0100

    Corrected lumi range for example Crab3 Config Data file.

--- SingleTop/test/Crab3Config_Data.py
+++ SingleTop/test/Crab3Config_Data.py
@@ -12,7 +12,7 @@ config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 10
 config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_v2.txt'
-config.Data.runRange = '256630-258158' #Run2015C 253659-256464; Run2015D 256630-260627
+config.Data.runRange = '253659-256464' #Run2015C 253659-256464; Run2015D 256630-260627
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
 config.Data.publication = False
