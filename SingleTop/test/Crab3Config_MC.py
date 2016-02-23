753d5cd8d580cb55adcc15e25d284724b0786acb
Alexander Morton <alexander.david.morton.cern.ch>
2016-02-23 17:46:21 +0100

    Removed redundant PU reweighting root files and removed references to them.

--- SingleTop/test/Crab3Config_MC.py
+++ SingleTop/test/Crab3Config_MC.py
@@ -2,21 +2,20 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 
 config = config()
 
-config.General.requestName = 'ZZTo4L_160108'
+config.General.requestName = 'WJetsToLNu_160215'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'nTupliserMC_miniAOD_cfg.py'
-config.JobType.inputFiles = ['pileup_MC_Summer12.root', 'run2012A_13Jul.root', 'run2012B_13Jul.root', 'run2012C_v2.root']
 
-config.Data.inputDataset = '/ZZTo4L_13TeV_powheg_pythia8/RunIIFall15MiniAODv1-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM'
+config.Data.inputDataset = '/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
 config.Data.publication = False
-config.Data.outputDatasetTag = 'CRAB3_MC_nTupilisation_ZZTo4L_160108'
+config.Data.outputDatasetTag = 'CRAB3_MC_nTupilisation_WJetsToLNu_160215'
 
 config.Site.storageSite = 'T2_UK_London_Brunel'
 
