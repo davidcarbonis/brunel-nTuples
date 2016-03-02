3e53364f3480eedfea0d7dddc70fe487e74771db
Alexander Morton <alexander.david.morton.cern.ch>
2016-03-02 16:46:59 +0100

    Updated Data crab3 config file.

--- SingleTop/test/Crab3Config_Data.py
+++ SingleTop/test/Crab3Config_Data.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'DoubleEG_Run2015C_16Dec2015_Golden38T_Data_160108'
+config.General.requestName = 'DoubleEG_Run2015C_16Dec2015_Golden38T_Data_160229'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
@@ -11,11 +11,11 @@ config.Data.inputDataset = '/DoubleEG/Run2015C_25ns-16Dec2015-v1/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 10
-config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_v2.txt'
-config.Data.runRange = '253659-256464' #Run2015C 253659-256464; Run2015D 256630-260627
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON.txt'
+config.Data.runRange = '254227-255031' #Run2015C 254227-255031; Run2015D 256630-260627; Run2015D without bad beamspot reco runs 256630-259625,259627-259635,259638-259680,259684,259686-260627
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
 config.Data.publication = False
-config.Data.outputDatasetTag = 'CRAB3_Data_nTupilisation_DoubleEG_Run2015C_16Dec2015_Golden38T_160108'
+config.Data.outputDatasetTag = 'CRAB3_Data_nTupilisation_DoubleEG_Run2015C_16Dec2015_Golden38T_160229'
 
 config.Site.storageSite = 'T2_UK_London_Brunel'
