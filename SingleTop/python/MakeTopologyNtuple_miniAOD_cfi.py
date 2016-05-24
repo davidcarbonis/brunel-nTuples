5d0624dfe4ac68326605d0c6b0569886a5775332
Alexander <alexander.david.morton@cern.ch>
2016-05-24 15:15:33 +0100

    Updated incorrect 2016 HLT paths.

--- SingleTop/python/MakeTopologyNtuple_miniAOD_cfi.py
+++ SingleTop/python/MakeTopologyNtuple_miniAOD_cfi.py
@@ -36,11 +36,12 @@ makeTopologyNtupleMiniAOD = cms.EDAnalyzer('MakeTopologyNtupleMiniAOD',
                                            
                                            triggerList = cms.vstring(                                                              
 	#Updated Triggers for 2016
-        'HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v3', #DoubleElectron
+        'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v3', #DoubleElectron
         'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v2', #DoubleMuon
         'HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v2', #DoubleMuon
-        'HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v3', #Muon+Electron
-        'HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v3', #Muon+Electron
+        'HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_v3', #Muon+Electron
+        'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v3', #Muon+Electron
+        'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v3', #Muon+Electron
         ),
                                            l1TriggerTag = cms.InputTag("gtDigis"),                                    
                                            checkTriggers = cms.bool(True),
