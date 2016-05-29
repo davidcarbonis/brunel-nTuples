246ee72c30db9099df55b22a5e3a2fc0708c034d
Alexander <alexander.david.morton@cern.ch>
2016-05-29 13:16:00 +0100

    Updated HLT list for the recommended top triggers for 2016B.

--- SingleTop/python/MakeTopologyNtuple_miniAOD_cfi.py
+++ SingleTop/python/MakeTopologyNtuple_miniAOD_cfi.py
@@ -36,12 +36,14 @@ makeTopologyNtupleMiniAOD = cms.EDAnalyzer('MakeTopologyNtupleMiniAOD',
                                            
                                            triggerList = cms.vstring(                                                              
 	#Updated Triggers for 2016
-        'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v3', #DoubleElectron
-        'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v2', #DoubleMuon
-        'HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v2', #DoubleMuon
-        'HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_v3', #Muon+Electron
-        'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v3', #Muon+Electron
-        'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v3', #Muon+Electron
+	'HLT_Ele23_WPLoose_Gsf_v3', 				#SingleElectron
+	'HLT_IsoMu18_v2',					#SingleMuon
+	'HLT_IsoTkMu18_v2',					#SinglueMuon
+	'HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v3', 	#DoubleElectron
+	'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v2', 		#DoubleMuon
+	'HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v2', 		#DoubleMuon
+	'HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v3', 	#Muon+Electron
+	'HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v3', 	#Muon+Electron
         ),
                                            l1TriggerTag = cms.InputTag("gtDigis"),                                    
                                            checkTriggers = cms.bool(True),
