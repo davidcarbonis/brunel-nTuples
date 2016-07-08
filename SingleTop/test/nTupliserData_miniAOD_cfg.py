13d1c05baa6cd3f6d88c83a122527c801d39ee4e
Alexander <alexander.david.morton@cern.ch>
2016-07-08 20:35:45 +0100

    Updated GTs in config files.

--- SingleTop/test/nTupliserData_miniAOD_cfg.py
+++ SingleTop/test/nTupliserData_miniAOD_cfg.py
@@ -44,7 +44,7 @@ process.options = cms.untracked.PSet(
                      )
 
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag.globaltag = cms.string('80X_dataRun2_Prompt_v8')
+process.GlobalTag.globaltag = cms.string('80X_dataRun2_Prompt_ICHEP16JEC_v0')
 
 #There's a bit in here about some btau tags that the code looks for. I don't know if this is significant, however. I'm going to ignore it for now.
 
