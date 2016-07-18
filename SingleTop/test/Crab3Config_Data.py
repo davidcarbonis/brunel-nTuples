5ef8f18f20a7b6c8f3beadb6523d6e255d4936f2
Alexander <alexander.david.morton@cern.ch>
2016-07-18 14:07:18 +0100

    Updated Run info in Crab3 config file.

--- SingleTop/test/Crab3Config_Data.py
+++ SingleTop/test/Crab3Config_Data.py
@@ -1,21 +1,21 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'DoubleEG_Run2016C_PromptReco_Golden38T_Data_160708'
+config.General.requestName = 'MET_Run2016B_PromptReco_Golden38T_Data_160718'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'nTupliserData_miniAOD_cfg.py'
 
-config.Data.inputDataset = '/DoubleEG/Run2016C-PromptReco-v2/MINIAOD'
+config.Data.inputDataset = '/MET/Run2016B-PromptReco-v2/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 10
 config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/Cert_271036-275783_13TeV_PromptReco_Collisions16_JSON.txt'
-config.Data.runRange = '275657-276283' #Run2016B 272007-275376; Run2016C 275657-276283; Run2016D 276315-
+config.Data.runRange = '272007-275376' #Run2016B 272007-275376; Run2016C 275657-276283; Run2016D 276315-276811; Run2016E 276831-
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
 config.Data.publication = False
-config.Data.outputDatasetTag = 'CRAB3_Data_nTupilisation_DoubleEG_Run2016C_PromptReco_Golden38T_160708'
+config.Data.outputDatasetTag = 'CRAB3_Data_nTupilisation_MET_Run2016B_PromptReco_Golden38T_160709'
 
 config.Site.storageSite = 'T2_UK_London_Brunel'
