89ae29873f8a56b5f062048471fae93567f92439
Alexander Morton <alexander.david.morton.cern.ch>
2016-09-28 13:29:43 +0200

    Optimised LHE to AOD Crab3 submission parameters.

--- FCNC/test/Crab3Config_LHEtoAOD.py
+++ FCNC/test/Crab3Config_LHEtoAOD.py
@@ -2,23 +2,24 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 
 config = config()
 
-config.General.requestName = 'TT_TopLeptonicDecay_TZ_2L_Kappa_Zct_160920'
+config.General.requestName = 'ST_TZ_2L_Zeta_Zut_160927'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
 config.JobType.pluginName = 'PrivateMC'
-config.JobType.psetName = 'prodLHEtoAOD_TT_TopLeptonicDecay_TZ_2L_Kappa_Zct.py'
+config.JobType.psetName = 'prodLHEtoAOD_ST_TZ_2L_Zeta_Zut.py'
 config.JobType.maxMemoryMB = 2500
 
-config.Data.outputPrimaryDataset = 'TT_TopLeptonicDecay_TZ_2L_Kappa_Zct_160920'
+config.Data.outputPrimaryDataset = 'ST_TZ_2L_Zeta_Zut_160927'
 config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 5000
-NJOBS = 1000  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.unitsPerJob = 500
+NJOBS = 5000  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
 config.Data.publication = True
-config.Data.outputDatasetTag = 'CRAB3_MC_TT_TopLeptonicDecay_TZ_2L_Kappa_Zct_160920'
+config.Data.outputDatasetTag = 'CRAB3_MC_ST_TZ_2L_Zeta_Zut_160927'
 
 config.Site.storageSite = 'T2_UK_London_Brunel'
+#config.Site.blacklist = ['T2_UK_London_Brunel']
