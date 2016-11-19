6893580054f8de9b6ba27405b7e7e493d804fd6e
Alexander <alexander.david.morton@cern.ch>
2016-11-19 16:47:05 +0000

    Bug fixing akin to branch 8_0_12

--- SingleTop/test/Crab3Config_Data.py
+++ SingleTop/test/Crab3Config_Data.py
@@ -1,17 +1,17 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'MET_Run2016B_PromptRecoV2_Golden38T_Data_161021'
+config.General.requestName = 'DoubleEG_Run2016B_23Sep2016_Golden38T_Data_161118'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'nTupliserData_miniAOD_cfg.py'
 
-config.Data.inputDataset = '/MET/Run2016B-PromptReco-v2/MINIAOD'
+config.Data.inputDataset = '/DoubleEG/Run2016B-23Sep2016-v3/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 10
-config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON.txt'
+config.Data.lumiMask = '/afs/cern.ch/user/j/jfernan/public/TOPtriggerJSONS2016/TOP_Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON_NoL1T_UnprescaledPaths.txt'
 
 ## ICHEP
 # Golden JSON: /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON.txt
@@ -25,6 +25,6 @@ config.Data.runRange = '272007-275376' #Run2016B 272007-275376; Run2016C 275657-
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
 config.Data.publication = False
-config.Data.outputDatasetTag = 'CRAB3_Data_nTupilisation_MET_Run2016B_PromptRecoV2_Golden38T_161021'
+config.Data.outputDatasetTag = 'CRAB3_Data_nTupilisation_DoubleEG_Run2016B_23Sep2016_Golden38T_161118'
 
 config.Site.storageSite = 'T2_UK_London_Brunel'
