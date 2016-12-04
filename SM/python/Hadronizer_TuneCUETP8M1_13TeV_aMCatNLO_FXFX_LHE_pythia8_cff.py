e4fca09ea2ed785a63e677fa4c231aeef2c703dc
Alexander <alexander.david.morton@cern.ch>
2016-12-04 18:34:42 +0000

    Added hadronizer file for tZq->qq

---
+++ SM/python/Hadronizer_TuneCUETP8M1_13TeV_aMCatNLO_FXFX_LHE_pythia8_cff.py
@@ -0,0 +1,58 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+process.generator = cms.EDFilter("Pythia8HadronizerFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.0),
+    maxEventsToPrint = cms.untracked.int32(1),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2', 
+            'Main:timesAllowErrors = 10000', 
+            'Check:epTolErr = 0.01', 
+            'Beams:setProductionScalesFromLHEF = off', 
+            'SLHA:keepSM = on', 
+            'SLHA:minMassSM = 1000.', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tau0Max = 10', 
+            'ParticleDecays:allowPhotonRadiation = on'),
+        pythia8CUEP8M1Settings = cms.vstring('Tune:pp 14', 
+            'Tune:ee 7', 
+            'MultipartonInteractions:pT0Ref=2.4024', 
+            'MultipartonInteractions:ecmPow=0.25208', 
+            'MultipartonInteractions:expPow=1.6'),
+        pythia8aMCatNLOSettings = cms.vstring('SpaceShower:pTmaxMatch = 1', 
+            'SpaceShower:pTmaxFudge = 1', 
+            'SpaceShower:MEcorrections = off', 
+            'TimeShower:pTmaxMatch = 1', 
+            'TimeShower:pTmaxFudge = 1', 
+            'TimeShower:MEcorrections = off', 
+            'TimeShower:globalRecoil = on', 
+            'TimeShower:limitPTmaxGlobal = on', 
+            'TimeShower:nMaxGlobalRecoil = 1', 
+            'TimeShower:globalRecoilMode = 2', 
+            'TimeShower:nMaxGlobalBranch = 1', 
+            'TimeShower:weightGluonToQuark = 1'),
+        processParameters = cms.vstring('JetMatching:setMad = off', 
+            'JetMatching:scheme = 1', 
+            'JetMatching:merge = on', 
+            'JetMatching:jetAlgorithm = 2', 
+            'JetMatching:etaJetMax = 999.', 
+            'JetMatching:coneRadius = 1.', 
+            'JetMatching:slowJetPower = 1', 
+            'JetMatching:qCut = 30.', 
+            'JetMatching:doFxFx = on', 
+            'JetMatching:qCutME = 20.', 
+            'JetMatching:nQmatch = 5', 
+            'JetMatching:nJetMax = 1'),
+        parameterSets = cms.vstring('pythia8CommonSettings', 
+            'pythia8CUEP8M1Settings', 
+            'pythia8aMCatNLOSettings', 
+            'processParameters')
+    )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
+
