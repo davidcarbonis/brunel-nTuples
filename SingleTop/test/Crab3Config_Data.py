d82d252839a0a405f72e1bcc37dcfedf5524a414
Alexander <alexander.david.morton@cern.ch>
2017-02-04 21:19:19 +0000

    Updated electron safe json mask.

--- SingleTop/test/Crab3Config_Data.py
+++ SingleTop/test/Crab3Config_Data.py
@@ -1,16 +1,16 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'MuonEG_Run2016B_23Sep2016_Golden38T_Data_161215'
+config.General.requestName = 'MET_Run2016B_23Sep2016_Golden38T_Data_170204'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'nTupliserData_miniAOD_cfg.py'
 
-config.Data.inputDataset = '/MuonEG/Run2016B-23Sep2016-v3/MINIAOD'
+config.Data.inputDataset = '/MET/Run2016B-23Sep2016-v3/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
-config.Data.unitsPerJob = 10
+config.Data.unitsPerJob = 100
 config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
 
 ## ICHEP
@@ -19,12 +19,11 @@ config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Coll
 
 ## Full year
 # Golden JSON: /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt
-# NoL1T Unprescaled (for Single + Double EG): /afs/cern.ch/user/j/jfernan/public/TOPtriggerJSONS2016/5Dic/ReducedJSONS_from_FullReRecoCertFile/LSfor_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_withLowestSeed_L1_DoubleEG_23_10.json
+# NoL1T Unprescaled (for Single + Double EG): /afs/cern.ch/user/j/jfernan/public/TOPtriggerJSONS2016/5Dic/ReducedJSONS_from_FullReRecoCertFile/LSforPath_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_withLowestSeed_L1_DoubleEG_23_10_OR_L1_DoubleEG_22_12.json
 
 config.Data.runRange = '272007-275376' #Run2016B 272007-275376; Run2016C 275657-276283; Run2016D 276315-276811; Run2016E 276831-277420; Run2016F 277772-278808; Run2016G 278820-280385; Run2016H 280919-284044;
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
-
 config.Data.publication = False
-config.Data.outputDatasetTag = 'CRAB3_Data_nTupilisation_MuonEG_Run2016B_23Sep2016_Golden38T_161215'
+config.Data.outputDatasetTag = 'CRAB3_Data_nTupilisation_MET_Run2016B_23Sep2016_Golden38T_170204'
 
 config.Site.storageSite = 'T2_UK_London_Brunel'
