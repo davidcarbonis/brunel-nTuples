a2f564e2cc742143f3ff266ee1619d348b29b41d
lunik1 <lunik1@users.noreply.github.com>
2018-07-13 14:46:30 +0100

    Update Data config for 2017

--- SingleTop/test/Crab3Config_Data.py
+++ SingleTop/test/Crab3Config_Data.py
@@ -6,17 +6,17 @@ time = datetime.now().strftime("%Y%m%d%H%M%S")
 
 config = config()
 
-config.General.requestName = "2017_Golden38T_Data__{}".format(time)
+config.General.requestName = "2017F_DoubleEG_Golden38T_Data__{}".format(time)
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'nTupliserData_miniAOD_cfg.py'
 
-config.Data.inputDataset = '/SingleElectron/Run2016B-03Feb2017_ver2-v2/MINIAOD'
+config.Data.inputDataset = '/DoubleEG/Run2017F-31Mar2018-v1/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 100
-config.Data.lumiMask = '/afs/cern.ch/user/j/jfernan/public/TOPtriggerJSONS2016/5Dic/ReducedJSONS_from_FullReRecoCertFile/LSforPath_HLT_Ele32_eta2p1_WPTight_Gsf_withLowestSeed_L1_SingleIsoEG22er_OR_L1_SingleIsoEG24er_OR_L1_SingleIsoEG26er_OR_L1_SingleIsoEG28er_OR_L1_SingleIsoEG30er_OR_L1_SingleIsoEG32er.json'
+config.Data.lumiMask = '/afs/cern.ch/user/c/choad/public/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt'
 
 ## ICHEP
 # Golden JSON: /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON.txt
@@ -28,9 +28,9 @@ config.Data.lumiMask = '/afs/cern.ch/user/j/jfernan/public/TOPtriggerJSONS2016/5
 # NoL1T Unprescaled (for Single EG): /afs/cern.ch/user/j/jfernan/public/TOPtriggerJSONS2016/5Dic/ReducedJSONS_from_FullReRecoCertFile/LSforPath_HLT_Ele32_eta2p1_WPTight_Gsf_withLowestSeed_L1_SingleIsoEG22er_OR_L1_SingleIsoEG24er_OR_L1_SingleIsoEG26er_OR_L1_SingleIsoEG28er_OR_L1_SingleIsoEG30er.json
 # NoL1T Unprescaled + L1_SingleIsoEG32er (for Single EG): /afs/cern.ch/user/j/jfernan/public/TOPtriggerJSONS2016/5Dic/ReducedJSONS_from_FullReRecoCertFile/LSforPath_HLT_Ele32_eta2p1_WPTight_Gsf_withLowestSeed_L1_SingleIsoEG22er_OR_L1_SingleIsoEG24er_OR_L1_SingleIsoEG26er_OR_L1_SingleIsoEG28er_OR_L1_SingleIsoEG30er_OR_L1_SingleIsoEG32er.json
 
-config.Data.runRange = "294927-306462"
+# config.Data.runRange = "294927-306462"
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = False
-config.Data.outputDatasetTag = "CRAB3_Data_nTupilisation_2017_Golden38T_Data__{}"
+config.Data.outputDatasetTag = "CRAB3_Data_nTupilisation_2017F_DoubleEG_Golden38T_Data__{}".format(time)
 
-config.Site.storageSite = 'T2_UK_London_IC' #'T2_UK_London_Brunel','T2_UK_London_IC'
+config.Site.storageSite = 'T2_UK_London_Brunel' #'T2_UK_London_Brunel','T2_UK_London_IC'
